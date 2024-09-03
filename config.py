d32f07239b965c2a1411f4bab316f6130a0ef27d
Huang <418618674@qq.com>
2024-09-03 23:01:38 +0800

    alpha

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+import util
+mac = util.get_local_mac()
+mac = mac.replace(":","")
+key = mac
 ##port
 serve_port = 8000
 ##API
 api_host = "http://81.70.52.233/api/aiot/"
-key = "112233445566"
-secret = "112233445566AIoT!@#123"
+key = "112233445566" #注释掉本行获取本机mac地址作为key
+secret = key+"AIoT!@#123"
 ##SSH
 ssh_username = "user"
 ssh_password = "password"
